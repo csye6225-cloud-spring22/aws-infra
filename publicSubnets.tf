5f972904f6a794ed9c0f4ae21493008a10b4b128
namrata_ruchandani_002125637 <ruchandani.n@northeastern.edu>
2023-02-23 00:02:35 -0500

    adding assignment4

--- publicSubnets.tf
+++ publicSubnets.tf
@@ -1,31 +1,32 @@
 resource "aws_subnet" "public_subnet_zone_1" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_public_cidr_zone1
-  availability_zone =data.aws_availability_zones.availableZone.names[0]
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_public_cidr_zone1
+  availability_zone       = data.aws_availability_zones.availableZone.names[0]
+  map_public_ip_on_launch = true
 
-  tags={
-    Name="A03 terraform public_subnet_zone_1"
+  tags = {
+    Name = "A03 terraform public_subnet_zone_1"
   }
 }
 
 
 resource "aws_subnet" "public_subnet_zone_2" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_public_cidr_zone2
-  availability_zone = data.aws_availability_zones.availableZone.names[1]
-
-  tags={
-    Name="A03 terraform public_subnet_zone_2"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_public_cidr_zone2
+  availability_zone       = data.aws_availability_zones.availableZone.names[1]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "A03 terraform public_subnet_zone_2"
   }
 }
 
 
 resource "aws_subnet" "public_subnet_zone_3" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.subnet_public_cidr_zone3
-  availability_zone =data.aws_availability_zones.availableZone.names[2]
-
-  tags={
-    Name="A03 terraform public_subnet_zone_3"
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.subnet_public_cidr_zone3
+  availability_zone       = data.aws_availability_zones.availableZone.names[2]
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "A03 terraform public_subnet_zone_3"
   }
 }
--- route_table.tf
+++ route_table.tf
@@ -22,70 +22,70 @@ resource "aws_route_table" "private1" {
 
 #zone 1 public subnet
 resource "aws_route_table_association" "public_z1" {
-    depends_on = [
+  depends_on = [
     aws_route_table.public1,
     aws_subnet.public_subnet_zone_1
   ]
 
-  route_table_id= aws_route_table.public1.id
-  subnet_id= aws_subnet.public_subnet_zone_1.id
+  route_table_id = aws_route_table.public1.id
+  subnet_id      = aws_subnet.public_subnet_zone_1.id
 }
 
 #zone 2 public subnet
 resource "aws_route_table_association" "public_z2" {
-     depends_on = [
+  depends_on = [
     aws_route_table.public1,
     aws_subnet.public_subnet_zone_2
   ]
 
-  route_table_id= aws_route_table.public1.id
-  subnet_id= aws_subnet.public_subnet_zone_2.id
+  route_table_id = aws_route_table.public1.id
+  subnet_id      = aws_subnet.public_subnet_zone_2.id
 }
 
 #zone 3 public subnets
 resource "aws_route_table_association" "public_z3" {
-     depends_on = [
+  depends_on = [
     aws_route_table.public1,
     aws_subnet.public_subnet_zone_2
   ]
 
-  route_table_id= aws_route_table.public1.id
-  subnet_id= aws_subnet.public_subnet_zone_3.id
+  route_table_id = aws_route_table.public1.id
+  subnet_id      = aws_subnet.public_subnet_zone_3.id
 }
 
 
 #zone 1 private subnets
 resource "aws_route_table_association" "private_z1" {
-     depends_on = [
+  depends_on = [
     aws_route_table.private1,
     aws_subnet.private_subnet_zone_1
   ]
 
-  route_table_id= aws_route_table.private1.id
-  subnet_id=aws_subnet.private_subnet_zone_1.id
+  route_table_id = aws_route_table.private1.id
+  subnet_id      = aws_subnet.private_subnet_zone_1.id
 }
 
 
 #zone 2 private subnets
 resource "aws_route_table_association" "private_z2" {
-     depends_on = [
+  depends_on = [
     aws_route_table.private1,
     aws_subnet.private_subnet_zone_2
   ]
 
-  route_table_id= aws_route_table.private1.id
-  subnet_id=aws_subnet.private_subnet_zone_2.id
+  route_table_id = aws_route_table.private1.id
+  subnet_id      = aws_subnet.private_subnet_zone_2.id
 }
 
 #zone 3 private subnets
 resource "aws_route_table_association" "private_z3" {
-     depends_on = [
+  depends_on = [
     aws_route_table.private1,
     aws_subnet.private_subnet_zone_3
   ]
 
-  route_table_id= aws_route_table.private1.id
-  subnet_id=aws_subnet.private_subnet_zone_3.id
+  route_table_id = aws_route_table.private1.id
+  subnet_id      = aws_subnet.private_subnet_zone_3.id
 }
 
 
